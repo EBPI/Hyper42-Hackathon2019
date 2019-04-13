aa8fee2d6a3dc638261ff44e76ae45448f4a0f21
rmulsa <rodrigo.mulsa@ebpi.nl>
2019-04-13 22:38:59 +0200

    Roles permissions per claim are saved

--- Phone/app/src/main/java/nl/hyper42/kim/android/ClaimIndDetails.java
+++ Phone/app/src/main/java/nl/hyper42/kim/android/ClaimIndDetails.java
@@ -19,17 +19,27 @@ public class ClaimIndDetails extends AppCompatActivity {
     String[] claims = {"Are you older than 18 years old?",
                         "Are you an EU citizen?","Are you you flying outside EU?",
                         "Which level of SkyBlue are you?"};
-    int claim_id;
+    int[] permissions;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_claim_ind_details);
         Toolbar toolbar = findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
-        claim_id = getIntent().getIntExtra("claim_id", -1);
-        TextView claimText = findViewById(R.id.claim_text_details);
-        claimText.setText(claims[claim_id-1]);//Index from 0 in array and claim ids start at 1 (sorry used to python now)
 
+        permissions = getIntent().getIntArrayExtra("permissions");
+        TextView claimText = findViewById(R.id.claim_text_details);
+        claimText.setText(claims[permissions[0]]);//Index from 0 in array and claim ids start at 1 (sorry used to python now)
+        CheckBox checkBox = findViewById(R.id.role_1_checkbox);
+        checkBox.setChecked(permissions[1]==1);
+        checkBox = findViewById(R.id.role_2_checkbox);
+        checkBox.setChecked(permissions[2]==1);
+        checkBox = findViewById(R.id.role_3_checkbox);
+        checkBox.setChecked(permissions[3]==1);
+        checkBox = findViewById(R.id.role_4_checkbox);
+        checkBox.setChecked(permissions[4]==1);
+        checkBox = findViewById(R.id.role_5_checkbox);
+        checkBox.setChecked(permissions[5]==1);
 
         //Find confirm button, when clicked, the activity ends and go back to prev. screen
         Button btn = findViewById(R.id.button2);
@@ -37,8 +47,7 @@ public class ClaimIndDetails extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intentWithResult = new Intent();
-                int[] checkboxes = new int[6];
-                checkboxes[0] = claim_id;
+                int[] checkboxes = permissions;
                 CheckBox checkBox = findViewById(R.id.role_1_checkbox);
                 checkboxes[1]=  (checkBox.isChecked()) ? 1 : 0;
                 checkBox = findViewById(R.id.role_2_checkbox);
